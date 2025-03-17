2602e833e8379cb21006a715049debf62fc77d25
Arafat Amin <arafat15-5321@diu.edu.bd>
2025-03-17 20:40:59 +0600

    initialCommit

---
+++ STCS/src/STSC.Infrastructure/InfrastructureModule.cs
@@ -0,0 +1,18 @@
+﻿using Autofac;
+
+public class InfrastructureModule : Module
+{
+    private readonly string _connectionString;
+    private readonly string _migrationAssemblyName;
+
+    public InfrastructureModule(string connectionString, string migrationAssemblyName)
+    {
+        _connectionString = connectionString;
+        _migrationAssemblyName = migrationAssemblyName;
+    }
+
+    protected override void Load(ContainerBuilder builder)
+    {
+        base.Load(builder);
+    }
+}
